03a4117fb04210f6bb2a180652e0ad7a7538195b
seonjuleee <nlsj0820@naver.com>
2021-11-22 05:18:38 +0900

    init

---
+++ src/main/java/com/meonji/meonjisroom/MeonjiSRoomApplication.java
@@ -0,0 +1,13 @@
+package com.meonji.meonjisroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeonjiSRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeonjiSRoomApplication.class, args);
+	}
+
+}
